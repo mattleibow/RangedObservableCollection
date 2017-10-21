32ec6113dfd13a68528b89ac7f8d434dd8cb1377
Matthew Leibowitz <mattleibow@live.com>
2017-10-21 05:17:15 +0200

    added docs

--- RangedObservableCollection/RangedObservableCollection.cs
+++ RangedObservableCollection/RangedObservableCollection.cs
@@ -4,21 +4,49 @@ using System.ComponentModel;
 
 namespace System.Collections.ObjectModel
 {
+	/// <summary>
+	/// Implementation of a dynamic data collection based on generic Collection&lt;T&gt;,
+	/// implementing INotifyCollectionChanged to notify listeners
+	/// when items get added, removed or the whole list is refreshed.
+	/// </summary>
 	public class RangedObservableCollection<T> : ObservableCollection<T>
 	{
 		private const string CountName = "Count";
 		private const string IndexerName = "Item[]";
 
+		/// <summary>
+		/// Initializes a new instance of RangedObservableCollection that is empty and has default initial capacity.
+		/// </summary>
 		public RangedObservableCollection()
 			: base()
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the RangedObservableCollection class that contains
+		/// elements copied from the specified collection and has sufficient capacity
+		/// to accommodate the number of elements copied.
+		/// </summary>
+		/// <param name="collection">The collection whose elements are copied to the new list.</param>
+		/// <remarks>
+		/// The elements are copied onto the RangedObservableCollection in the
+		/// same order they are read by the enumerator of the collection.
+		/// </remarks>
+		/// <exception cref="ArgumentNullException"> collection is a null reference </exception>
 		public RangedObservableCollection(IEnumerable<T> collection)
 			: base(collection)
 		{
 		}
 
+		/// <summary>
+		/// Adds the elements of the specified collection to the end of the RangedObservableCollection.
+		/// </summary>
+		/// <param name="collection">The collection whose elements are added to the list.</param>
+		/// <remarks>
+		/// The elements are copied onto the RangedObservableCollection in the
+		/// same order they are read by the enumerator of the collection.
+		/// </remarks>
+		/// <exception cref="ArgumentNullException"> collection is a null reference </exception>
 		public void AddRange(IEnumerable<T> collection)
 		{
 			if (collection == null)
@@ -37,6 +65,15 @@ namespace System.Collections.ObjectModel
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, changedItems, startIndex));
 		}
 
+		/// <summary>
+		/// Clears the current RangedObservableCollection and replaces the elements with the elements of specified collection.
+		/// </summary>
+		/// <param name="collection">The collection whose elements are added to the list.</param>
+		/// <remarks>
+		/// The elements are copied onto the RangedObservableCollection in the
+		/// same order they are read by the enumerator of the collection.
+		/// </remarks>
+		/// <exception cref="ArgumentNullException"> collection is a null reference </exception>
 		public void ReplaceRange(IEnumerable<T> collection)
 		{
 			if (collection == null)
@@ -56,6 +93,15 @@ namespace System.Collections.ObjectModel
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, changedItems, oldItems));
 		}
 
+		/// <summary>
+		/// Removes the first occurence of each item in the specified collection.
+		/// </summary>
+		/// <param name="collection">The collection whose elements are removed from list.</param>
+		/// <remarks>
+		/// The elements are copied onto the RangedObservableCollection in the
+		/// same order they are read by the enumerator of the collection.
+		/// </remarks>
+		/// <exception cref="ArgumentNullException"> collection is a null reference </exception>
 		public void RemoveRange(IEnumerable<T> collection)
 		{
 			if (collection == null)
@@ -77,10 +123,14 @@ namespace System.Collections.ObjectModel
 			{
 				OnPropertyChanged(new PropertyChangedEventArgs(CountName));
 				OnPropertyChanged(new PropertyChangedEventArgs(IndexerName));
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, changedItems, -1));
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, changedItems));
 			}
 		}
 
+		/// <summary>
+		/// Clears the current RangedObservableCollection and replaces the elements with the specified element.
+		/// </summary>
+		/// <param name="item">The element which is added to the list.</param>
 		public void Replace(T item)
 		{
 			CheckReentrancy();
